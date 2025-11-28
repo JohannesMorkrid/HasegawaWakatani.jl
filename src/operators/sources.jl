b0e626baa784f893e96f88773d448c622e674228
JohannesMorkrid <jomor9370@uit.no>
2025-11-28 10:16:00 +0100

    Draft on how Source operators should work

--- src/operators/sources.jl
+++ src/operators/sources.jl
@@ -2,8 +2,12 @@
 #                                          Sources                                          
 # ------------------------------------------------------------------------------------------
 
-struct Source <: SpectralOperator
+struct ConstantSource <: SpectralOperator
     source::AbstractArray
+end
+
+function build_operator(::Val{:constant_source}, domain::AbstractDomain; shape, kwargs...)
+    ConstantSource(shape(domain, kwargs...))
 end
 
 struct SpectralConstant <: SpectralOperator
@@ -20,6 +24,55 @@ function build_operator(::Val{:spectral_constant}, domain::AbstractDomain; kwarg
     SpectralConstant(domain)
 end
 
-# -------------------------------------- Main Method ---------------------------------------
+struct Source <: SpectralOperator
+    shape::AbstractArray
+end
 
+# ------------------------------------- Main Methods ---------------------------------------
+
+# To allow for adding a constant
 @inline (op::SpectralConstant)(constant::Number) = constant .* op.delta
+
+# To allow for adding constant source
+@inline Base.:+(field::AbstractArray, op::ConstantSource) = field .+ op.source
+@inline Base.:+(op::ConstantSource, field::AbstractArray) = field .+ op.source
+@inline (op::ConstantSource)() = op.source
+
+# To allow for field dependent, i.e. density dependent sources
+@inline (op::Source)(field::AbstractArray) = op.quadratic_term(op.shape, field)
+@inline (op::ConstantSource)(field::AbstractArray) = op.constant .* field
+
+# TODO allow for time and field dependent sources
+#@inline (op::Source)(field::AbstractArray, time::Number) = op.quadratic_term(op.shape(domain,
+#                                                                                      t),
+#                                                                             field)
+#@inline (op::Source)(time::Number) = (op.shape(domain, t), field)
+
+# """
+# # Field in-dependent source:
+# eq... + source
+# eq... + source()
+
+# # Field dependent source:
+# eq... + source(f)
+# """
+
+# function NonLinear(du, u, operators, p, t)
+#     @unpack solve_phi, poisson_bracket, diff_y, quadratic_term = operators
+#     @unpack spectral_exp, spectral_expm1, spectral_log, = operators
+#     @unpack g, σ_0 = p
+#     n, Ω = eachslice(u; dims=3)
+#     dn, dΩ = eachslice(du; dims=3)
+#     ϕ = solve_phi(Ω)
+
+#     dn .= poisson_bracket(n, ϕ) - g * diff_y(n) + g * quadratic_term(n, diff_y(ϕ)) -
+#           σ_0 * quadratic_term(n, spectral_exp(-ϕ)) + source(n, t)
+#     dΩ .= poisson_bracket(Ω, ϕ) - g * diff_y(spectral_log(n)) - σ_0 * spectral_expm1(-ϕ)
+# end
+
+# S = ConstantSource(exp.(rand(256, 256)))
+# field = rand(256, 256)
+
+# field + S
+# field + S()
+# field + field
