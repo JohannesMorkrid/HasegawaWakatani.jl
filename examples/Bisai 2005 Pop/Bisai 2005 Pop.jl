13862d5ca08b7ea7f02b13d4d006d499207cb4f8
JohannesMorkrid <jomor9370@uit.no>
2025-11-28 10:16:33 +0100

    Playing around with sources in Bisai example, still a draft

--- examples/Bisai 2005 Pop/Bisai 2005 Pop.jl
+++ examples/Bisai 2005 Pop/Bisai 2005 Pop.jl
@@ -3,7 +3,7 @@ using HasegawaWakatani
 using CUDA
 
 ## Run scheme test for Burgers equation
-domain = Domain(128, 128; Lx=160, Ly=160, MemoryType=CuArray, precision=Float32)
+domain = Domain(128, 128; Lx=160, Ly=160, MemoryType=CuArray, precision=Float64)
 ic = initial_condition(random_crossphased, domain; value=1e-3)
 ic[:, :, 1] .+= 0.5
 
@@ -18,13 +18,6 @@ function Linear(du, u, operators, p, t)
     dΩ .= μ * laplacian(Ω)
 end
 
-# Current implementation of source
-source(x, y, S_0, λ_s) = @. S_0 * exp(-((x + 80 - λ_s) / λ_s)^2) + 0 * y
-
-S = get_fwd(domain) * CuArray(source(domain.x', domain.y, 5e-4, 5))
-using Plots
-heatmap(Array(get_bwd(domain) * S); aspect_ratio=:equal)
-
 # Non-linear operator, fully non-linear
 function NonLinear(du, u, operators, p, t)
     @unpack solve_phi, poisson_bracket, diff_y, quadratic_term = operators
@@ -35,20 +28,21 @@ function NonLinear(du, u, operators, p, t)
     ϕ = solve_phi(Ω)
 
     dn .= poisson_bracket(n, ϕ) - g * diff_y(n) + g * quadratic_term(n, diff_y(ϕ)) -
-          σ_0 * quadratic_term(n, spectral_exp(-ϕ)) .+ S#.+ source()
+          σ_0 * quadratic_term(n, spectral_exp(-ϕ)) .+ S#+ source
     dΩ .= poisson_bracket(Ω, ϕ) - g * diff_y(spectral_log(n)) - σ_0 * spectral_expm1(-ϕ)
 end
 
 # Parameters
-parameters = (μ=1e-2, ν=1e-2, g=8e-4, σ_0=2e-4, S_0=5e-4, λ_s=5.0)
+parameters = (μ=1e-2, ν=1e-2, g=8e-4, σ_0=2e-4, S_0=5e-4, λ_s=5.0, λ_θ=10)
 
-tspan = [0.0, 50_000_000.0]
+tspan = [0.0, 5_000_000.0]
 
 # Diagnostics
 diagnostics = @diagnostics [
     progress(; stride=1000),
     #probe_all(; positions=[(x, 0) for x in LinRange(-80, 64, 10)], stride=10),
-    plot_density(; stride=50)
+    plot_density(; stride=50),
+    sample_density(; stride=50)
     #radial_flux(; stride=50),
     #kinetic_energy_integral(; stride=50),
     #potential_energy_integral(; stride=50),
@@ -61,8 +55,20 @@ diagnostics = @diagnostics [
     #kinetic_energy_spectrum(; spectrum=:poloidal, stride=50)
 ]
 
+# Current implementation of source
+function source(x, y, S_0, λ_s, λ_θ)
+    @. S_0 * (0.1 * sin(λ_θ * y) + 1) * exp(-((x + 80 - λ_s / 2) / λ_s)^2) + 0 * y
+end
+
+S = get_fwd(domain) * CuArray(source(domain.x', domain.y, 5e-4, 60, 30))
+using Plots
+heatmap(Array(get_bwd(domain) * S); aspect_ratio=:equal)
+
 prob = SpectralODEProblem(Linear, NonLinear, ic, domain, tspan; p=parameters, dt=1,
-                          operators=:all, diagnostics=diagnostics)
+                          operators=:all, diagnostics=diagnostics,
+                          #additional_operators=[OperatorRecipe(:constant_source;
+                          #shape=source)]
+                          )
 
 # Output
 output_file_name = joinpath(@__DIR__, "output", "Bisai.h5")
@@ -74,3 +80,9 @@ sol = spectral_solve(prob, MSS3(), output; resume=true)
 
 # bisai_php_19_052509 gives Lₓ = L_y = 160ρₛ, g = 1e-3, 128-modes
 # bisai_php_12_072520 gives dt = \omega_c^{-1}, g = 8e-4
+
+using Statistics
+using Plots
+
+density = output.simulation["Density/data"][:, :, 1:3000]
+plot(mean(mean(density; dims=3); dims=1)[:])
